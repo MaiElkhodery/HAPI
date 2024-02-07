caec25b0092d20bcf28dc96152be82414e4cb987
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-02-07 21:06:14 +0200

    modify content logic

--- app/src/main/java/com/example/hapi/presentation/signup/progress/ProgressScreen.kt
+++ app/src/main/java/com/example/hapi/presentation/signup/progress/ProgressScreen.kt
@@ -15,9 +15,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -26,7 +26,10 @@ import com.example.hapi.presentation.signup.farmersignup.ui.navToFarmerSignup
 import com.example.hapi.ui.theme.GreenAppColor
 
 @Composable
-fun ProgressScreen(navController: NavController) {
+fun ProgressScreen(
+    navController: NavController,
+    isFinal: Boolean = false
+) {
 
     var continueClicked by remember {
         mutableStateOf(false)
@@ -68,21 +71,30 @@ fun ProgressScreen(navController: NavController) {
                 },
             contentAlignment = Alignment.Center
         ) {
-            if (continueClicked) {
-                IdentitySelection(
-                    onClickLandowner = {
-                        //TODO:NAV TO LANDOWNER SIGN UP
-                    },
-                    onclickFarmer = {
-                        navController.navToFarmerSignup()
-                    }
-                )
+            if (isFinal) {
+                SetupMessage(
+                    message = stringResource(id = R.string.congratulation)
+                ) {
+                    //TODO:NAV TO HOME
+                }
             } else {
-                SetupMessage {
-                    continueClicked = true
+                if (continueClicked) {
+                    IdentitySelection(
+                        onClickLandowner = {
+                            //TODO:NAV TO LANDOWNER SIGN UP
+                        },
+                        onclickFarmer = {
+                            navController.navToFarmerSignup()
+                        }
+                    )
+                } else {
+                    SetupMessage(
+                        message = stringResource(id = R.string.account_setup)
+                    ) {
+                        continueClicked = true
+                    }
                 }
             }
-
         }
         Box(
             modifier = Modifier

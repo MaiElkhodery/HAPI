e5d541a562f503c787107e706e5e3a180ef2cbb5
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-02-08 22:00:55 +0200

    add landownerSignupRoute

---
+++ app/src/main/java/com/example/hapi/presentation/signup/landownersignup/ui/LandownerSignupRoute.kt
@@ -0,0 +1,16 @@
+package com.example.hapi.presentation.signup.landownersignup.ui
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+
+private const val ROUTE = "landowner_signup"
+fun NavGraphBuilder.landownerSignupRoute(navController: NavController) {
+    composable(route = ROUTE) {
+        LandownerSignup(navController)
+    }
+}
+
+fun NavController.navToLandownerSignup() {
+    navigate(ROUTE)
+}

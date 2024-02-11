80ead54ff4aaced92408297151ef2958be6826a0
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-02-11 20:42:17 +0200

    modify parameters passed to final screen

--- app/src/main/java/com/example/hapi/presentation/signup/landownersignup/ui/cropdetection/FinalCropRoute.kt
+++ app/src/main/java/com/example/hapi/presentation/signup/landownersignup/ui/cropdetection/FinalCropRoute.kt
@@ -9,7 +9,7 @@ private const val ROUTE = "final_crop/{crop_name}/{crop_imageId}"
 fun NavGraphBuilder.finalCropRoute(navController: NavController) {
     composable(route = ROUTE) { backEntryStack ->
         FinalCropScreen(
-            navController, Crop(
+            navController = navController, crop = Crop(
                 name = backEntryStack.arguments?.getString("cropName") ?: "",
                 image = backEntryStack.arguments?.getInt("cropId") ?: 0
             )

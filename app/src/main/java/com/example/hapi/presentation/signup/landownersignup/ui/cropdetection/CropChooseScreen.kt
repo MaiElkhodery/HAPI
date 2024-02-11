0394bc5420facd9cf6a10f6333394c38122847af
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-02-11 20:24:29 +0200

    add choose available crops

---
+++ app/src/main/java/com/example/hapi/presentation/signup/landownersignup/ui/cropdetection/CropChooseScreen.kt
@@ -0,0 +1,75 @@
+package com.example.hapi.presentation.signup.landownersignup.ui.cropdetection
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.hapi.R
+import com.example.hapi.data.model.crops
+import com.example.hapi.presentation.signup.common.SignupAndGuestHeader
+import com.example.hapi.presentation.signup.landownersignup.ui.info.LotusRow
+import com.example.hapi.ui.theme.GreenAppColor
+
+@Composable
+fun CropChooseScreen(
+    navController: NavController
+) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(GreenAppColor)
+            .padding(horizontal = 26.dp)
+    ) {
+
+        val (header, content, title, lotusRow) = createRefs()
+        val topGuideLine = createGuidelineFromTop(.08f)
+        val bottomGuideLine = createGuidelineFromBottom(.11f)
+
+        SignupAndGuestHeader(
+            modifier = Modifier.constrainAs(header) {
+                top.linkTo(topGuideLine)
+            },
+            topText = stringResource(id = R.string.setting_up),
+            downText = stringResource(id = R.string.your_account)
+        )
+
+        Title(title = stringResource(id = R.string.avilable_crop),
+            modifier = Modifier.constrainAs(title) {
+                top.linkTo(header.bottom)
+                bottom.linkTo(content.top)
+            })
+
+        CropChooseContent(
+            modifier = Modifier
+                .constrainAs(content) {
+                    top.linkTo(title.bottom)
+                    bottom.linkTo(lotusRow.top)
+                },
+            crops = crops
+        ) { crop ->
+            navController.navToFinalCropScreen(crop)
+        }
+
+        LotusRow(
+            highlightedLotusPos = 1,
+            modifier = Modifier
+                .constrainAs(lotusRow) {
+                    top.linkTo(content.bottom)
+                    bottom.linkTo(bottomGuideLine)
+                }
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CropChoosePreview() {
+    CropChooseScreen(rememberNavController())
+}

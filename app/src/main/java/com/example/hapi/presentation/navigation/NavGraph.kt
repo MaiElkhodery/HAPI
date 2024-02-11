6a0426fefd1cbe2ee9859186ed5ddd9620634534
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-02-11 20:26:42 +0200

    add routes in NavGraph

--- app/src/main/java/com/example/hapi/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/example/hapi/presentation/navigation/NavGraph.kt
@@ -4,8 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.example.hapi.presentation.signup.farmersignup.ui.farmerSignupRoute
-import com.example.hapi.presentation.signup.landownersignup.ui.cropRecommendationRoute
-import com.example.hapi.presentation.signup.landownersignup.ui.landownerSignupRoute
+import com.example.hapi.presentation.signup.landownersignup.ui.cropdetection.cropChooseRoute
+import com.example.hapi.presentation.signup.landownersignup.ui.cropdetection.cropDetectionRoute
+import com.example.hapi.presentation.signup.landownersignup.ui.cropdetection.finalCropRoute
+import com.example.hapi.presentation.signup.landownersignup.ui.info.landownerSignupRoute
 import com.example.hapi.presentation.signup.progress.progressRoute
 import com.example.hapi.presentation.splash.splashOneRoute
 
@@ -17,6 +19,8 @@ fun NavGraph(navController: NavHostController) {
         progressRoute(navController)
         farmerSignupRoute(navController)
         landownerSignupRoute(navController)
-        cropRecommendationRoute(navController)
+        cropDetectionRoute(navController)
+        cropChooseRoute(navController)
+        finalCropRoute(navController)
     }
 }

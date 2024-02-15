c70d4427b4ec22b5208d8946be762c06cb22944a
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-02-15 14:02:20 +0200

    add screens

--- app/src/main/java/com/example/hapi/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/example/hapi/presentation/navigation/NavGraph.kt
@@ -4,23 +4,28 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.example.hapi.presentation.signup.farmersignup.ui.farmerSignupRoute
-import com.example.hapi.presentation.signup.landownersignup.ui.cropdetection.cropChooseRoute
-import com.example.hapi.presentation.signup.landownersignup.ui.cropdetection.cropDetectionRoute
-import com.example.hapi.presentation.signup.landownersignup.ui.cropdetection.finalCropRoute
-import com.example.hapi.presentation.signup.landownersignup.ui.info.landownerSignupRoute
+import com.example.hapi.presentation.signup.landownersignup.choose.cropChooseRoute
+import com.example.hapi.presentation.signup.landownersignup.detection.cropDetectionRoute
+import com.example.hapi.presentation.signup.landownersignup.finalcrop.finalCropRoute
+import com.example.hapi.presentation.signup.landownersignup.info.landownerSignupRoute
+import com.example.hapi.presentation.signup.landownersignup.recommendation.cropRecommendationRoute
+import com.example.hapi.presentation.signup.progress.identitySelectionRoute
 import com.example.hapi.presentation.signup.progress.progressRoute
 import com.example.hapi.presentation.splash.splashOneRoute
 
 @Composable
 fun NavGraph(navController: NavHostController) {
-    val startRoute = "splash"
+    val startRoute = "progress/{final}"
     NavHost(navController = navController, startDestination = startRoute) {
+        progressRoute(navController)
         splashOneRoute(navController)
         progressRoute(navController)
+        identitySelectionRoute(navController)
         farmerSignupRoute(navController)
         landownerSignupRoute(navController)
         cropDetectionRoute(navController)
         cropChooseRoute(navController)
+        cropRecommendationRoute(navController)
         finalCropRoute(navController)
     }
 }

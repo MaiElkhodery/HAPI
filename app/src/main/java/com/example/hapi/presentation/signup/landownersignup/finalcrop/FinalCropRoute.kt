926e31739bc68934ec07f37bea50a29717eeff6d
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-02-15 13:57:57 +0200

    change in passed crop class

--- app/src/main/java/com/example/hapi/presentation/signup/landownersignup/finalcrop/FinalCropRoute.kt
+++ app/src/main/java/com/example/hapi/presentation/signup/landownersignup/finalcrop/FinalCropRoute.kt
@@ -1,22 +1,23 @@
-package com.example.hapi.presentation.signup.landownersignup.ui.cropdetection
+package com.example.hapi.presentation.signup.landownersignup.finalcrop
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.example.hapi.data.model.Crop
+import com.example.hapi.data.model.CropType
 
 private const val ROUTE = "final_crop/{crop_name}/{crop_imageId}"
 fun NavGraphBuilder.finalCropRoute(navController: NavController) {
     composable(route = ROUTE) { backEntryStack ->
         FinalCropScreen(
             navController = navController, crop = Crop(
-                name = backEntryStack.arguments?.getString("cropName") ?: "",
-                image = backEntryStack.arguments?.getInt("cropId") ?: 0
+                type = CropType.valueOf(backEntryStack.arguments?.getString("cropName") ?: ""),
+                imageId = backEntryStack.arguments?.getInt("cropId") ?: 0
             )
         )
     }
 }
 
 fun NavController.navToFinalCropScreen(crop: Crop) {
-    navigate("${ROUTE}/${crop.name}/${crop.image}")
+    navigate("$ROUTE/${crop.type.name}/${crop.imageId}")
 }

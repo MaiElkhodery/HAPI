bc07c0ec9d39150b08236a32f3eeb8ee39401486
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-02-15 13:55:55 +0200

    remove ChosenCropText fun and replace it with YellowText fun

---
+++ app/src/main/java/com/example/hapi/presentation/signup/common/HapiText.kt
@@ -0,0 +1,31 @@
+package com.example.hapi.presentation.signup.common
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.example.hapi.R
+import com.example.hapi.ui.theme.YellowAppColor
+
+@Composable
+fun YellowText(
+    modifier: Modifier = Modifier,
+    size: Int,
+    text: String
+) {
+    Text(
+        modifier = modifier,
+        color = YellowAppColor,
+        fontSize = size.sp,
+        fontFamily = FontFamily(
+            Font(
+                R.font.poppins_black
+            )
+        ),
+        text = text,
+        textAlign = TextAlign.Center
+    )
+}
--- app/src/main/java/com/example/hapi/presentation/signup/landownersignup/finalcrop/FinalCropContent.kt
+++ app/src/main/java/com/example/hapi/presentation/signup/landownersignup/finalcrop/FinalCropContent.kt
@@ -1,4 +1,4 @@
-package com.example.hapi.presentation.signup.landownersignup.ui.cropdetection
+package com.example.hapi.presentation.signup.landownersignup.finalcrop
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
@@ -6,40 +6,37 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.hapi.R
 import com.example.hapi.data.model.Crop
+import com.example.hapi.data.model.CropType
 import com.example.hapi.presentation.signup.common.ConfirmButton
-import com.example.hapi.ui.theme.YellowAppColor
+import com.example.hapi.presentation.signup.common.YellowText
 
 @Composable
 fun FinalCropContent(
     modifier: Modifier,
     crop: Crop,
-    onClick:()->Unit
-){
+    onClick: () -> Unit
+) {
     Column(
-        modifier=modifier.fillMaxWidth(),
+        modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         ChosenCrop(crop = crop)
         Spacer(modifier = Modifier.padding(21.dp))
-        ConfirmButton( text = stringResource(id = R.string.confirm_signup)) {
+        ConfirmButton(text = stringResource(id = R.string.confirm_signup)) {
             onClick()
         }
     }
 }
+
 @Composable
 private fun ChosenCrop(
     crop: Crop
@@ -47,38 +44,27 @@ private fun ChosenCrop(
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ChosenCropText(text = stringResource(id = R.string.your_crop))
+        YellowText(text = stringResource(id = R.string.your_crop), size = 20)
 
         Image(
             modifier = Modifier
                 .size(95.dp)
                 .padding(vertical = 6.dp),
-            painter = painterResource(id = crop.image),
+            painter = painterResource(id = crop.imageId),
             contentDescription = "crop image"
         )
-        ChosenCropText(text = crop.name)
+        YellowText(size = 20, text = crop.type.name)
 
     }
-}
-
-@Composable
-private fun ChosenCropText(text: String) {
-    Text(
-        color = YellowAppColor,
-        fontSize = 20.sp,
-        fontFamily = FontFamily(
-            Font(
-                R.font.poppins_black
-            )
-        ), text = text, textAlign = TextAlign.Center
-    )
 }
 
 @Preview
 @Composable
 private fun ChosenCropPreview() {
-    FinalCropContent(Modifier, crop = Crop(
-        name = "TOMATO",
-        image = R.drawable.tomato
-    )){}
+    FinalCropContent(
+        Modifier, crop = Crop(
+            type = CropType.TOMATO,
+            imageId = R.drawable.tomato
+        )
+    ) {}
 }

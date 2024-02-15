851def27d7bfe3e295d895b12e1a7e65dfeb1066
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-02-15 22:13:30 +0200

    add data store

---
+++ app/src/main/java/com/example/hapi/data/local/AuthPreference.kt
@@ -0,0 +1,17 @@
+package com.example.hapi.data.local
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import com.example.hapi.util.AUTH_KEY
+import javax.inject.Inject
+
+class AuthPreference @Inject constructor(
+    private val dataStore: DataStore<Preferences>
+) {
+    suspend fun saveAuthToken(token: String) {
+        dataStore.edit { pref ->
+            pref[AUTH_KEY] = setOf(token)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hapi/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.hapi.util
+
+import androidx.datastore.preferences.core.stringSetPreferencesKey
+
+const val AUTH_PREFERENCES = "AUTH_PREF"
+val AUTH_KEY = stringSetPreferencesKey("auth_key")

06eaa17382251450490d3eb1e8a527425f76a984
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-02-23 20:02:13 +0200

    refactoring

--- app/src/main/java/com/example/hapi/presentation/auth/signup/landownersignup/detection/CropDetectionContent.kt
+++ app/src/main/java/com/example/hapi/presentation/auth/signup/landownersignup/detection/CropDetectionContent.kt
@@ -21,7 +21,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hapi.R
-import com.example.hapi.presentation.signup.common.GreenBlackText
+import com.example.hapi.presentation.auth.signup.common.GreenBlackText
 import com.example.hapi.ui.theme.YellowAppColor
 
 

529b8f2ec9bea07ef8fd5c794daf2baf76e5311e
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-02-27 10:59:03 +0200

    add saveRole function and getToken, getRole

--- app/src/main/java/com/example/hapi/data/local/AuthPreference.kt
+++ app/src/main/java/com/example/hapi/data/local/AuthPreference.kt
@@ -4,6 +4,8 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import com.example.hapi.util.AUTH_KEY
+import com.example.hapi.util.ROLE_KEY
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class AuthPreference @Inject constructor(
@@ -13,5 +15,20 @@ class AuthPreference @Inject constructor(
         dataStore.edit { pref ->
             pref[AUTH_KEY] = setOf(token)
         }
+    }
+    suspend fun saveRole(role: String) {
+        dataStore.edit { pref ->
+            pref[ROLE_KEY] = setOf(role)
+        }
+    }
+
+    suspend fun getToken(): String? {
+        val preferences = dataStore.data.first()
+        return preferences[AUTH_KEY]?.first()
+    }
+
+    suspend fun getRole(): String? {
+        val preferences = dataStore.data.first()
+        return preferences[ROLE_KEY]?.first()
     }
 }

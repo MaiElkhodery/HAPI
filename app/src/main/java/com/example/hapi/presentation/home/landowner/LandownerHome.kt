e9fb628c854315e922a5a5996f100c4b6aa9bc5b
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-03 17:10:41 +0200

    add components of landowner home

--- app/src/main/java/com/example/hapi/presentation/home/landowner/LandownerHome.kt
+++ app/src/main/java/com/example/hapi/presentation/home/landowner/LandownerHome.kt
@@ -1,24 +1,59 @@
 package com.example.hapi.presentation.home.landowner
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.hapi.R
 import com.example.hapi.data.model.Detection
-import com.example.hapi.data.model.Disease
-import com.example.hapi.presentation.home.DetectionAndLandFeatureRow
-import com.example.hapi.presentation.home.HomeHeader
+import com.example.hapi.presentation.home.common.CustomNavigationBottom
+import com.example.hapi.presentation.home.common.CustomNavigationBottomBackground
+import com.example.hapi.presentation.home.common.HistoryCard
+import com.example.hapi.presentation.home.common.HomeHeader
+import com.example.hapi.presentation.home.common.LandData
+import com.example.hapi.presentation.home.detectionhistory.navigateToDetectionHistory
+import com.example.hapi.ui.theme.GreenAppColor
 import com.example.hapi.util.LandAction
 
 @Composable
-fun LandownerHome() {
-    Column {
+fun LandownerHome(
+    navController: NavController
+) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(GreenAppColor)
+    ) {
 
-        HomeHeader(imageId = R.drawable.user_photo, username = "ahmed") {
+        val (header, content, historyCards, navBottom,bottomBackground) = createRefs()
+        val topGuideLine = createGuidelineFromTop(.07f)
 
-        }
+        HomeHeader(
+            modifier = Modifier
+                .constrainAs(header) {
+                    top.linkTo(topGuideLine)
+                    bottom.linkTo(content.top)
+                },
+            imageId = R.drawable.user_img,
+            username = "khaled"
+        )
 
-        DetectionAndLandFeatureRow(
+        LandData(
+            modifier = Modifier
+                .padding(horizontal = 32.dp)
+                .constrainAs(content) {
+                    top.linkTo(header.bottom, margin = 21.dp)
+                    bottom.linkTo(historyCards.top, margin = 22.dp)
+                },
             lastLandAction = com.example.hapi.data.model.LandAction(
                 LandAction.FERTILIZATION.name,
                 "2021-09-01",
@@ -28,16 +63,63 @@ fun LandownerHome() {
                 username = "John Doe",
                 date = "12/12/2021",
                 time = "12:00",
-                image_path = "",
-                possible_diseases = emptyList(),
+                imagePath = "",
+                possibleDiseases = emptyList(),
                 crop = "",
             )
         )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 26.dp)
+                .constrainAs(historyCards) {
+                    top.linkTo(content.bottom)
+                    bottom.linkTo(navBottom.top, margin = 26.dp)
+                },
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            HistoryCard(
+                type = "land",
+                modifier = Modifier.weight(1f)
+
+            ) {
+
+            }
+            HistoryCard(
+                type = "detection",
+                modifier = Modifier.weight(1f)
+
+            ) {
+                navController.navigateToDetectionHistory()
+            }
+        }
+        CustomNavigationBottomBackground(
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(bottomBackground) {
+                    bottom.linkTo(parent.bottom)
+                }
+        )
+        CustomNavigationBottom(
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .constrainAs(navBottom) {
+                    bottom.linkTo(parent.bottom)
+                },
+            onHomeClick = { },
+            onCameraClick = { },
+            onSettingsClick = { }
+        )
+
     }
+
 }
 
 @Preview
 @Composable
 fun LandownerHomePreview() {
-    LandownerHome()
+    LandownerHome(
+        rememberNavController()
+    )
 }

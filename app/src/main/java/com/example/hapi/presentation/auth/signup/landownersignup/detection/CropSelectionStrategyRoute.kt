b8aac35976d2b47b4b8637705ba46baa97f7369f
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-07 00:48:56 +0200

    modify methods names and add getRecommendedCrops method

--- app/src/main/java/com/example/hapi/presentation/auth/signup/landownersignup/detection/CropSelectionStrategyRoute.kt
+++ app/src/main/java/com/example/hapi/presentation/auth/signup/landownersignup/detection/CropSelectionStrategyRoute.kt
@@ -4,16 +4,16 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 
-private const val ROUTE = "crop_detection"
-fun NavGraphBuilder.cropDetectionRoute(navController: NavController) {
+private const val ROUTE = "crop_selection_strategy"
+fun NavGraphBuilder.cropSelectionStrategyRoute(navController: NavController) {
     composable(route = ROUTE) {
-        CropDetectionScreen(navController)
+        CropSelectionStrategy(navController)
     }
 }
 
-fun NavController.navToCropDetection() {
-    navigate(ROUTE){
-        popUpTo(ROUTE){
+fun NavController.navigateToCropSelectionStrategy() {
+    navigate(ROUTE) {
+        popUpTo(ROUTE) {
             inclusive = true
         }
     }

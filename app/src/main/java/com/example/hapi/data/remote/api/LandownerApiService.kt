7ea8793991d0e4c7e6cbc76bb37fe15e50930561
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-07 00:51:51 +0200

    add cropRecommendation and uploadSelectedCrop

--- app/src/main/java/com/example/hapi/data/remote/api/LandownerApiService.kt
+++ app/src/main/java/com/example/hapi/data/remote/api/LandownerApiService.kt
@@ -1,5 +1,20 @@
 package com.example.hapi.data.remote.api
 
-interface LandownerApiService {
+import com.example.hapi.data.remote.request.SelectCropRequest
+import com.example.hapi.data.remote.response.CropRecommendationResponse
+import com.example.hapi.util.RECOMMENDATIONS
+import com.example.hapi.util.SELECTED_CROP
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
 
+interface LandownerApiService {
+    @POST(RECOMMENDATIONS)
+    suspend fun getRecommendedCrops(
+    ): Response<CropRecommendationResponse>
+
+    @POST(SELECTED_CROP)
+    suspend fun uploadSelectedCrop(
+        @Body crop: SelectCropRequest
+    ): Response<Unit>
 }
--- app/src/main/java/com/example/hapi/data/repository/LandownerRepository.kt
+++ app/src/main/java/com/example/hapi/data/repository/LandownerRepository.kt
@@ -1,12 +1,28 @@
 package com.example.hapi.data.repository
 
 import com.example.hapi.data.local.AuthPreference
+import com.example.hapi.data.model.State
+import com.example.hapi.data.remote.api.ApiHandler
 import com.example.hapi.data.remote.api.LandownerApiService
+import com.example.hapi.data.remote.request.SelectCropRequest
+import com.example.hapi.data.remote.response.CropRecommendationResponse
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class LandownerRepository @Inject constructor(
     private val landownerApiService: LandownerApiService,
     private val authPreference: AuthPreference
-) {
+) : ApiHandler() {
+    suspend fun cropRecommendation(): Flow<State<CropRecommendationResponse?>> {
+        return ApiHandler().makeRequest (
+            execute = { landownerApiService.getRecommendedCrops() },
+        )
+    }
 
+    suspend fun uploadSelectedCrop(crop: String): Flow<State<Unit?>> {
+        return ApiHandler().makeRequest (
+            execute = { landownerApiService.uploadSelectedCrop(SelectCropRequest(crop = crop)) },
+            onSuccess = { authPreference.saveHaveSelectedCrop(true) }
+        )
+    }
 }

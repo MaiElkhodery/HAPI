19c616e80162a63c610c224b334003b3cefe94d3
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-17 18:36:08 +0200

    update APIVars.kt

--- app/src/main/java/com/example/hapi/util/APIVars.kt
+++ app/src/main/java/com/example/hapi/util/APIVars.kt
@@ -1,6 +1,12 @@
 package com.example.hapi.util
 
-const val BASE_URL = "https://f921-154-237-242-25.ngrok-free.app"
+const val BASE_URL = "https://bb54-154-237-112-134.ngrok-free.app"
 const val LANDOWNER_SIGNUP = "$BASE_URL/api/signup/landowner"
 const val FARMER_SIGNUP = "$BASE_URL/api/signup/farmer"
 const val SIGNIN = "$BASE_URL/api/login"
+const val DETECTION = "$BASE_URL/api/detect"
+
+const val LANDOWNER_DETECTION_HISTORY = "$BASE_URL/api/detections"
+const val DETECTION_ITEM = "$BASE_URL/api/detections"
+const val LAST_DETECTION = "$BASE_URL/api/detections/latest"
+const val LAST_Five_DETECTION = "$BASE_URL/api/detections/latest-five"

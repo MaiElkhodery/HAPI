da328fe3c158312021a3304c14d36436cdfb3b81
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-17 13:53:08 +0200

    change location

--- app/src/main/java/com/example/hapi/data/local/datastore/AuthPreference.kt
+++ app/src/main/java/com/example/hapi/data/local/datastore/AuthPreference.kt
@@ -1,4 +1,4 @@
-package com.example.hapi.data.local
+package com.example.hapi.data.local.datastore
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences

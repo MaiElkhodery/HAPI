9fa296f52cefa02ca72431828c02d1bb8ed69fb0
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-17 18:17:48 +0200

    add createMultiPartBody, getCurrentDateAsString, getCurrentTimeAsString, uriToByteArray, Bitmap.toByteArray, ByteArray.toBitmap, downloadImage, isNetworkConnected, crossFadeAnimation functions

--- app/src/main/java/com/example/hapi/presentation/home/landowner/LandownerHome.kt
+++ app/src/main/java/com/example/hapi/presentation/home/landowner/LandownerHome.kt
@@ -7,28 +7,49 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.hapi.R
-import com.example.hapi.data.model.Detection
 import com.example.hapi.presentation.home.common.CustomNavigationBottom
 import com.example.hapi.presentation.home.common.CustomNavigationBottomBackground
 import com.example.hapi.presentation.home.common.HistoryCard
 import com.example.hapi.presentation.home.common.HomeHeader
-import com.example.hapi.presentation.home.common.LandData
+import com.example.hapi.presentation.home.common.HomeLandData
 import com.example.hapi.presentation.home.cropselection.navigateToCropSelection
+import com.example.hapi.presentation.home.detectiondetails.navigateToDetectionDetails
 import com.example.hapi.presentation.home.detectionhistory.navigateToDetectionHistory
 import com.example.hapi.ui.theme.GreenAppColor
 import com.example.hapi.util.LandAction
+import com.example.hapi.util.isNetworkConnected
 
 @Composable
 fun LandownerHome(
-    navController: NavController
+    navController: NavController,
+    viewModel: LandownerHomeViewModel = hiltViewModel()
 ) {
+    var isNetworkConnected by remember {
+        mutableStateOf(false)
+    }
+    LaunchedEffect(true) {
+        isNetworkConnected = isNetworkConnected()
+        if (isNetworkConnected) {
+            viewModel.getLastDetection()
+        } else {
+            viewModel.getLastLocalDetection()
+        }
+    }
+
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
@@ -42,32 +63,65 @@ fun LandownerHome(
             modifier = Modifier
                 .constrainAs(header) {
                     top.linkTo(topGuideLine)
+                    bottom.linkTo(content.top)
                 },
             imageId = R.drawable.user_img,
             username = "khaled"
         )
 
-        LandData(
-            modifier = Modifier
-                .padding(horizontal = 32.dp)
-                .constrainAs(content) {
-                    top.linkTo(header.bottom, margin = 21.dp)
-                    bottom.linkTo(historyCards.top)
-                },
-            lastLandAction = com.example.hapi.data.model.LandAction(
-                LandAction.FERTILIZATION.name,
-                "2021-09-01",
-                "12:00"
-            ),
-            lastDetection = Detection(
-                username = "John Doe",
-                date = "12/12/2021",
-                time = "12:00",
-                imagePath = "",
-                possibleDiseases = emptyList(),
-                crop = "",
-            )
-        )
+        if (isNetworkConnected) {
+            val lastDetection =
+                viewModel.lastDetection.collectAsState().value
+            if (lastDetection != null) {
+                HomeLandData(
+                    modifier = Modifier
+                        .padding(horizontal = 32.dp)
+                        .constrainAs(content) {
+                            top.linkTo(header.bottom, margin = 21.dp)
+                            bottom.linkTo(historyCards.top)
+                        },
+                    lastLandAction = com.example.hapi.domain.model.LandAction(
+                        LandAction.FERTILIZATION.name,
+                        "2021-09-01",
+                        "12:00"
+                    ),
+                    username = lastDetection.username,
+                    date = lastDetection.date,
+                    time = lastDetection.time,
+                    image_url = lastDetection.image_url
+                ) {
+                    navController.navigateToDetectionDetails(lastDetection.id.toString())
+                }
+            }
+
+        } else {
+            val lastDetection = viewModel.lastLocalDetection.collectAsState().value
+            if (lastDetection != null) {
+
+                HomeLandData(
+                    modifier = Modifier
+                        .padding(horizontal = 32.dp)
+                        .constrainAs(content) {
+                            top.linkTo(header.bottom, margin = 21.dp)
+                            bottom.linkTo(historyCards.top)
+                        },
+                    lastLandAction = com.example.hapi.domain.model.LandAction(
+                        LandAction.FERTILIZATION.name,
+                        "2021-09-01",
+                        "12:00"
+                    ),
+                    username = lastDetection.detection.name,
+                    date = lastDetection.detection.date,
+                    time = lastDetection.detection.time,
+                    image_url = "",
+                    byteArray = lastDetection.detection.imageByteArray
+                ) {
+                    navController.navigateToDetectionDetails(lastDetection.detection.detectionId.toString())
+                }
+            }
+        }
+
+
 
         Row(
             modifier = Modifier
@@ -75,7 +129,7 @@ fun LandownerHome(
                 .padding(horizontal = 26.dp)
                 .constrainAs(historyCards) {
                     top.linkTo(content.bottom, margin = 12.dp)
-                    bottom.linkTo(navBottom.top, margin = 28.dp)
+                    bottom.linkTo(navBottom.top, margin = 33.dp)
                 },
             horizontalArrangement = Arrangement.SpaceBetween
         ) {

e7b1906797bfc376ef852b2ea8028551617e90a0
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-17 18:20:17 +0200

    add LandownerHomeViewModel

---
+++ app/src/main/java/com/example/hapi/presentation/home/landowner/LandownerHomeViewModel.kt
@@ -0,0 +1,69 @@
+package com.example.hapi.presentation.home.landowner
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hapi.data.local.room.entities.history.DetectionHistoryItem
+import com.example.hapi.data.local.room.entities.history.DetectionHistoryWithDiseases
+import com.example.hapi.data.remote.response.DetectionHistoryResponse
+import com.example.hapi.domain.model.Detection
+import com.example.hapi.domain.model.State
+import com.example.hapi.domain.usecase.LastDetectionUseCase
+import com.example.hapi.domain.usecase.LocalDetectionsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LandownerHomeViewModel @Inject constructor(
+    private val getLastDetectionUseCase: LastDetectionUseCase,
+    private val localDetectionsUseCase: LocalDetectionsUseCase
+) : ViewModel() {
+    private val _loading = MutableStateFlow(false)
+    val loading = _loading.asStateFlow()
+
+    private val _errorMsg = MutableStateFlow("")
+    val errorMsg = _errorMsg.asStateFlow()
+
+    private val _lastDetection = MutableStateFlow(
+        DetectionHistoryResponse(0, "", "", "", "")
+    )
+    val lastDetection = _lastDetection.asStateFlow()
+
+    private val _lastLocalDetection = MutableStateFlow<DetectionHistoryWithDiseases?>(null)
+    val lastLocalDetection = _lastLocalDetection.asStateFlow()
+
+    fun getLastDetection() {
+        viewModelScope.launch {
+            getLastDetectionUseCase().collect { state ->
+                Log.d("LAST DETECTION", "viewmodel, $state")
+                when (state) {
+                    is State.Error -> {
+                        _loading.value = false
+                        _errorMsg.value = state.error.message
+
+                    }
+
+                    is State.Loading -> {
+                        _loading.value = true
+                    }
+
+                    is State.Success -> {
+                        _loading.value = false
+                        _lastDetection.value = state.result!!
+                    }
+
+                    else -> TODO()
+                }
+            }
+        }
+    }
+
+    suspend fun getLastLocalDetection() {
+        viewModelScope.launch {
+            _lastLocalDetection.value = localDetectionsUseCase().first()
+        }
+    }
+}

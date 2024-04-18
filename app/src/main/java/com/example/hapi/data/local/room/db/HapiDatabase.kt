38e4409a6fcd35d01dd25073dfdd172ae1178466
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-18 17:45:36 +0200

    update: add DetectionOfHistory Dao and Entity remove Detection and Disease Entities and Dao

--- app/src/main/java/com/example/hapi/data/local/room/db/HapiDatabase.kt
+++ app/src/main/java/com/example/hapi/data/local/room/db/HapiDatabase.kt
@@ -4,31 +4,26 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.hapi.data.local.room.dao.current_detection.CurrentDetectionDao
 import com.example.hapi.data.local.room.dao.current_detection.CurrentDiseaseDao
-import com.example.hapi.data.local.room.dao.current_detection.CurrrentDetectionDao
-import com.example.hapi.data.local.room.dao.history.DetectionDao
-import com.example.hapi.data.local.room.dao.history.DiseaseDao
+import com.example.hapi.data.local.room.dao.detection_history.DetectionOfHistoryDao
 import com.example.hapi.data.local.room.entities.current_detection.CurrentDetection
 import com.example.hapi.data.local.room.entities.current_detection.CurrentDetectionDisease
-import com.example.hapi.data.local.room.entities.history.Detection
-import com.example.hapi.data.local.room.entities.history.Disease
+import com.example.hapi.data.local.room.entities.detection_history.DetectionOfHistory
 import javax.inject.Singleton
 
 @Singleton
 @Database(
-    entities = [(CurrentDetection::class), (CurrentDetectionDisease::class),
-        (Detection::class), (Disease::class)],
+    entities = [(CurrentDetection::class), (CurrentDetectionDisease::class), (DetectionOfHistory::class)],
     version = 1,
     exportSchema = false
 )
 abstract class HapiDatabase : RoomDatabase() {
-    abstract fun currentDetectionDao(): CurrrentDetectionDao
+    abstract fun currentDetectionDao(): CurrentDetectionDao
 
     abstract fun currentDiseaseDao(): CurrentDiseaseDao
 
-    abstract fun detectionDao(): DetectionDao
-
-    abstract fun diseaseDao(): DiseaseDao
+    abstract fun detectionOfHistoryDao(): DetectionOfHistoryDao
 
     companion object {
         @Volatile

752a65c68e195dd2c308e06783176fa2f5772228
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-18 17:49:17 +0200

    remove id, username, image_url, time, date attributes

--- app/src/main/java/com/example/hapi/data/remote/response/DetectionResponse.kt
+++ app/src/main/java/com/example/hapi/data/remote/response/DetectionResponse.kt
@@ -3,11 +3,6 @@ package com.example.hapi.data.remote.response
 import com.example.hapi.domain.model.DetectionHistory
 
 data class DetectionResponse(
-    val id : Int,
-    val username: String,
-    val image_url: String,
     val detection: DetectionHistory,
-    val time: String,
-    val date: String,
     val crop: String
 )

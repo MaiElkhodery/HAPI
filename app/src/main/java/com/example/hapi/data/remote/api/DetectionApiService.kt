a4762e5d5f65e7681d12e197f284dd929f9c4786
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-18 17:46:36 +0200

    remove getLastDetection and getLastFiveDetections

--- app/src/main/java/com/example/hapi/data/remote/api/DetectionApiService.kt
+++ app/src/main/java/com/example/hapi/data/remote/api/DetectionApiService.kt
@@ -1,16 +1,16 @@
 package com.example.hapi.data.remote.api
 
+import com.example.hapi.data.remote.request.DetectionHistoryRequest
 import com.example.hapi.data.remote.response.DetectionHistoryResponse
 import com.example.hapi.data.remote.response.DetectionResponse
 import com.example.hapi.data.remote.response.DiseaseDetectionResponse
 import com.example.hapi.util.DETECTION
 import com.example.hapi.util.DETECTION_ITEM
-import com.example.hapi.util.LANDOWNER_DETECTION_HISTORY
-import com.example.hapi.util.LAST_DETECTION
-import com.example.hapi.util.LAST_Five_DETECTION
+import com.example.hapi.util.DETECTION_HISTORY
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
@@ -26,15 +26,12 @@ interface DetectionApiService {
         @Part("crop") crop: RequestBody
     ): Response<DiseaseDetectionResponse>
 
-    @GET(LANDOWNER_DETECTION_HISTORY)
-    suspend fun getDetectionHistory(): Response<List<DetectionHistoryResponse>>
+    @GET(DETECTION_HISTORY)
+    suspend fun getDetectionHistory(
+        @Body detectionHistoryRequest: DetectionHistoryRequest
+    ): Response<List<DetectionHistoryResponse>>
 
     @GET("$DETECTION_ITEM/{id}")
     suspend fun getDetection(@Path("id") id: Int): Response<DetectionResponse>
 
-    @GET(LAST_DETECTION)
-    suspend fun getLastDetection(): Response<DetectionHistoryResponse>
-
-    @GET(LAST_Five_DETECTION)
-    suspend fun getLastFiveDetections(): Response<List<DetectionResponse>>
 }

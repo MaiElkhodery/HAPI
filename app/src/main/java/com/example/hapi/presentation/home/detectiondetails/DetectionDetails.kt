0e17afb4eeec948a552fdcb10abddfd24dfdc6ea
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-18 18:03:15 +0200

    update ui

--- app/src/main/java/com/example/hapi/presentation/home/detectiondetails/DetectionDetails.kt
+++ app/src/main/java/com/example/hapi/presentation/home/detectiondetails/DetectionDetails.kt
@@ -2,7 +2,6 @@ package com.example.hapi.presentation.home.detectiondetails
 
 import android.content.Intent
 import android.net.Uri
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -20,9 +19,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,7 +34,6 @@ import androidx.core.content.ContextCompat.startActivity
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImage
 import com.example.hapi.R
 import com.example.hapi.presentation.auth.common.NavHeader
 import com.example.hapi.presentation.home.common.DetectionInfo
@@ -47,7 +42,6 @@ import com.example.hapi.presentation.home.common.DetectionResult
 import com.example.hapi.presentation.home.common.getCropIcon
 import com.example.hapi.ui.theme.GreenAppColor
 import com.example.hapi.ui.theme.YellowAppColor
-import com.example.hapi.util.BASE_URL
 import com.example.hapi.util.Crop
 import com.example.hapi.util.Dimens
 import com.example.hapi.util.YellowBlackText
@@ -57,19 +51,25 @@ import com.example.hapi.util.toBitmap
 fun DetectionDetails(
     navController: NavController,
     viewModel: DetectionDetailsViewModel = hiltViewModel(),
-    detectionId: Int,
-    local: Boolean = false
+    localId: String,
+    remoteId: String,
 ) {
-    val isLocal by remember {
-        mutableStateOf(local)
-    }
     LaunchedEffect(true) {
-        Log.d("DetectionDetails", "detectionId: $detectionId, isLocal: $isLocal")
-        if (isLocal) viewModel.getCachedDetection(detectionId)
-        else viewModel.getDetection(detectionId)
+        if (remoteId.isNotBlank()) {
+            viewModel.getLocalDetection(localId.toInt())
+            viewModel.getRemoteDetectionDetailsById(remoteId.toInt())
+        } else {
+            viewModel.getCachedDiseaseDetectionResult(localId.toInt())
+        }
     }
-    val remoteDetection = viewModel.remoteDetectionItem.collectAsState().value
-    val localDetection = viewModel.localDetection.collectAsState().value
+
+    val image = viewModel.byteArrayImage.collectAsState().value
+    val username = viewModel.username.collectAsState().value
+    val date = viewModel.date.collectAsState().value
+    val time = viewModel.time.collectAsState().value
+    val crop = viewModel.crop.collectAsState().value
+    val confidence = viewModel.confidence.collectAsState().value
+    val diseaseList = viewModel.diseaseList.collectAsState().value
 
     val context = LocalContext.current
 
@@ -92,154 +92,110 @@ fun DetectionDetails(
         ) {
             navController.popBackStack()
         }
-
-        if (remoteDetection != null || localDetection != null) {
-            Log.d("DetectionDetails", "localDetection: $localDetection")
-            Log.d("DetectionDetails", "remoteDetection: $remoteDetection")
-
-            Row(
-                modifier = Modifier
-                    .constrainAs(imageAndData) {
-                        top.linkTo(header.bottom, margin = 37.dp)
-                        bottom.linkTo(text.top)
-                    }
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Box(
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(8.dp))
-                        .border(width = 3.dp, color = YellowAppColor)
-                        .fillMaxWidth()
-                        .height(IntrinsicSize.Max)
-                        .weight(1.2f)
-
-                ) {
-                    if (!isLocal) {
-                        AsyncImage(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .height(200.dp),
-                            model = BASE_URL + remoteDetection!!.image_url,
-                            contentDescription = null,
-                            contentScale = ContentScale.FillBounds
-                        )
-                    } else {
-                        Image(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .height(200.dp),
-                            bitmap = localDetection!!.detection.image.toBitmap().asImageBitmap(),
-                            contentDescription = null,
-                            contentScale = ContentScale.FillBounds
-                        )
-                    }
+        Row(
+            modifier = Modifier
+                .constrainAs(imageAndData) {
+                    top.linkTo(header.bottom, margin = 37.dp)
+                    bottom.linkTo(text.top)
                 }
-
-                DetectionInfo(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 16.dp)
-                        .weight(1f),
-                    username = if (!isLocal) remoteDetection!!.username else localDetection!!.detection.detectionMaker,
-                    date = if (!isLocal) remoteDetection!!.date else localDetection!!.detection.date,
-                    time = if (!isLocal) remoteDetection!!.time else localDetection!!.detection.time,
-                    color = YellowAppColor,
-                    fontSize = 15
-                )
-            }
-
-            Row(
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
                 modifier = Modifier
-                    .constrainAs(text) {
-                        top.linkTo(imageAndData.bottom, margin = 22.dp)
-                        bottom.linkTo(boxes.top, margin = 22.dp)
-                    }
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                val crop =
-                    if (!isLocal) remoteDetection!!.crop.uppercase() else localDetection!!.detection.crop.uppercase()
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(width = 3.dp, color = YellowAppColor)
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Max)
+                    .weight(1.2f)
 
+            ) {
                 Image(
                     modifier = Modifier
-                        .padding(end = 8.dp)
-                        .size(31.dp),
-                    painter = painterResource(
-                        id = getCropIcon(
-                            Crop.valueOf(crop)
-                        )
-                    ),
-                    contentDescription = "crop icon"
+                        .fillMaxSize()
+                        .height(200.dp),
+                    bitmap = image.toBitmap().asImageBitmap(),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds
                 )
-                YellowBlackText(size = 24, text = crop)
+
             }
-            val isHealthy =
-                if (!isLocal) remoteDetection!!.detection.isHealthy else localDetection!!.detection.isHealthy
-            Log.d("DetectionDetails", "isHealthy: $isHealthy")
-            if (isHealthy) {
-                DetectionResult(
-                    name = stringResource(id = R.string.healthy),
-                    confidence = if (!isLocal) remoteDetection!!.detection.confidence.toString()
-                    else localDetection!!.detection.confidence.toString(),
-                    modifier = Modifier
-                        .constrainAs(boxes) {
 
-                            top.linkTo(text.bottom, margin = Dimens.content_margin)
-                            bottom.linkTo(bottomGuideLine)
+            DetectionInfo(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp)
+                    .weight(1f),
+                username = username,
+                date = date,
+                time = time,
+                color = YellowAppColor,
+                fontSize = 15
+            )
+        }
 
-                        }
-                        .padding(horizontal = 12.dp)
-                        .clip(RoundedCornerShape(5.dp)),
-                    alignment = Alignment.CenterHorizontally
-                )
+        Row(
+            modifier = Modifier
+                .constrainAs(text) {
+                    top.linkTo(imageAndData.bottom, margin = 22.dp)
+                    bottom.linkTo(boxes.top, margin = 22.dp)
+                }
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(31.dp),
+                painter = painterResource(
+                    id = getCropIcon(
+                        Crop.valueOf(crop.uppercase())
+                    )
+                ),
+                contentDescription = "crop icon"
+            )
+            YellowBlackText(size = 24, text = crop.uppercase())
+        }
+        if (diseaseList.isEmpty()) {
+            DetectionResult(
+                name = stringResource(id = R.string.healthy),
+                confidence = (confidence * 100).toInt().toString(),
+                modifier = Modifier
+                    .constrainAs(boxes) {
 
-            } else {
-                LazyColumn(
-                    modifier = Modifier
-                        .constrainAs(boxes) {
-
-                            top.linkTo(text.bottom, margin = Dimens.content_margin)
-                            bottom.linkTo(bottomGuideLine)
-
-                        },
-                ) {
-
-                    if (isLocal) {
-                        val diseases = localDetection!!.diseases
-                        items(diseases.size) { index ->
-                            val disease = diseases[index]
-                            DetectionLowConfidence(
-                                modifier = Modifier.padding(vertical = 8.dp),
-                                confidence = (disease.confidence * 100).toInt().toString(),
-                                name = disease.name,
-                            ) {
-                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(disease.infoLink))
-                                startActivity(context, intent, null)
-                            }
-                            Log.d("DetectionDetails", "confidence: ${disease.confidence.toInt()}")
-                        }
-                    } else {
-
-                        val diseases = remoteDetection!!.detection.diseases
-                        items(diseases.size) { index ->
-                            val disease = diseases[index]
-                            DetectionLowConfidence(
-                                modifier = Modifier.padding(vertical = 8.dp),
-                                confidence = (disease.confidence * 100).toInt().toString(),
-                                name = disease.name,
-                            ) {
-                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(disease.infoLink))
-                                startActivity(context, intent, null)
-                            }
-
-                            Log.d("DetectionDetails", "confidence: ${disease.confidence.toInt()}")
-                        }
+                        top.linkTo(text.bottom, margin = Dimens.content_margin)
+                        bottom.linkTo(bottomGuideLine)
 
                     }
-                }
+                    .padding(horizontal = 12.dp)
+                    .clip(RoundedCornerShape(5.dp)),
+                alignment = Alignment.CenterHorizontally
+            )
 
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .constrainAs(boxes) {
+
+                        top.linkTo(text.bottom, margin = Dimens.content_margin)
+                        bottom.linkTo(bottomGuideLine)
+
+                    },
+            ) {
+                items(diseaseList.size) { index ->
+                    val disease = diseaseList[index]
+                    DetectionLowConfidence(
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        confidence = (disease.confidence * 100).toInt().toString(),
+                        name = disease.name,
+                    ) {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(disease.infoLink))
+                        startActivity(context, intent, null)
+                    }
+                }
             }
+
+
         }
     }
 }
@@ -249,6 +205,7 @@ fun DetectionDetails(
 fun DetectionDetailsPreview() {
     DetectionDetails(
         rememberNavController(),
-        detectionId = 0
+        localId = "0",
+        remoteId = "0"
     )
 }

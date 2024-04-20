95a84cf7955001f9d0b0bbc1c89e45014439a7a8
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-20 18:23:11 +0200

    update get history method to get history and last detection while online and update the history id

--- app/src/main/java/com/example/hapi/presentation/home/landowner/LandownerHomeViewModel.kt
+++ app/src/main/java/com/example/hapi/presentation/home/landowner/LandownerHomeViewModel.kt
@@ -4,7 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.hapi.data.local.datastore.UserDataPreference
-import com.example.hapi.data.local.room.entities.detection_history.DetectionOfHistory
+import com.example.hapi.domain.model.State
 import com.example.hapi.domain.usecase.FetchNewestDetectionUseCase
 import com.example.hapi.domain.usecase.GetAndSaveDetectionHistoryUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -23,52 +23,68 @@ class LandownerHomeViewModel @Inject constructor(
     private val _errorMsg = MutableStateFlow("")
     val errorMsg = _errorMsg.asStateFlow()
 
-    private val _lastDetection = MutableStateFlow<DetectionOfHistory?>(null)
-    val lastDetection = _lastDetection.asStateFlow()
+    private val _imageUrl = MutableStateFlow("")
+    val imageUrl = _imageUrl.asStateFlow()
 
-    private val _id = MutableStateFlow(0)
-    val id = _id.asStateFlow()
+    private val _username = MutableStateFlow("")
+    val username = _username.asStateFlow()
+
+    private val _date = MutableStateFlow("")
+    val date = _date.asStateFlow()
+
+    private val _time = MutableStateFlow("")
+    val time = _time.asStateFlow()
+
+    private val _remoteId = MutableStateFlow(0)
+    val remoteId = _remoteId.asStateFlow()
+
+    private val _localId = MutableStateFlow(0)
+    val localId = _localId.asStateFlow()
 
 
-    fun getDetectionHistory(
-        id: Int
-    ) {
+    fun getDetectionHistory() {
         viewModelScope.launch {
-            getDetectionHistoryListUseCase(id).collect { state ->
-                Log.d("LAST DETECTION", "viewmodel, $state")
+            getDetectionHistoryListUseCase(
+                userDataPreference.getLastHistoryId().toInt()
+            ).collect { state ->
                 when (state) {
-                    true -> {
-                        Log.d("LAST DETECTION", "viewmodel, true")
+                    is State.Error -> {
+                        Log.d("LANDOWNER HOME", "ERROR")
                     }
 
-                    false -> {
-                        Log.d("LAST DETECTION", "viewmodel, false")
+                    is State.Exception -> {
+                        Log.d("LANDOWNER HOME", "EXCEPTION")
                     }
+
+                    State.Loading -> {
+                        Log.d("LANDOWNER HOME", "LOADING")
+                    }
+
+                    is State.Success -> {
+                        val detection = getNewestDetectionUseCase()
+                        userDataPreference.saveLastHistoryId(detection.remoteId.toString())
+                        _imageUrl.value = detection.imageUrl
+                        _username.value = detection.username
+                        _date.value = detection.date
+                        _time.value = detection.time
+                        _remoteId.value = detection.remoteId
+                        _localId.value = detection.id
+                    }
+
                 }
             }
         }
     }
 
-    suspend fun getLastDetection() {
+    suspend fun getLastDetectionAndSetLastId() {
         viewModelScope.launch {
-            _lastDetection.value = getNewestDetectionUseCase()
-            setId(_lastDetection.value!!.remoteId.toString())
+            val detection = getNewestDetectionUseCase()
+            _imageUrl.value = detection.imageUrl
+            _username.value = detection.username
+            _date.value = detection.date
+            _time.value = detection.time
+            _remoteId.value = detection.remoteId
+            _localId.value = detection.id
         }
-    }
-
-    private fun getId() {
-        viewModelScope.launch {
-            _id.value = userDataPreference.getLastHistoryId()!!.toInt()
-        }
-    }
-
-    private fun setId(lastId: String) {
-        viewModelScope.launch {
-            userDataPreference.saveLastHistoryId(lastId)
-        }
-    }
-
-    init {
-        getId()
     }
 }

4c7668abb7aa4d0d6d7980087474ac731ce2fd40
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-23 11:35:50 +0200

    add LandHistoryViewModel

---
+++ app/src/main/java/com/example/hapi/presentation/home/landhistory/LandHistoryViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.hapi.presentation.home.landhistory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hapi.data.local.room.entities.land_history.LandData
+import com.example.hapi.domain.usecase.GetLandDataUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LandHistoryViewModel @Inject constructor(
+    private val getLandDataUseCase: GetLandDataUseCase
+) : ViewModel() {
+
+    private val _landDataList = MutableStateFlow(emptyList<LandData>())
+    val landDataList = _landDataList.asStateFlow()
+
+    private val _errorMsg = MutableStateFlow("")
+    val errorMsg = _errorMsg.asStateFlow()
+
+    fun getLandHistory() {
+        viewModelScope.launch {
+            getLandDataUseCase()?.let {
+                _landDataList.value = it
+            }
+        }
+    }
+
+}

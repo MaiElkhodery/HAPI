8805d3cc40ddbd4e89b9eaf6b85f665b6a1f44d9
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-04-23 11:28:33 +0200

    add provideLandownerRepository, GetLandDataUseCase, GetLastLandDataUseCase

---
+++ app/src/main/java/com/example/hapi/domain/usecase/GetAndSaveLandDataUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.hapi.domain.usecase
+
+import com.example.hapi.data.repository.LandownerRepository
+import javax.inject.Inject
+
+class c @Inject constructor(
+    private val landownerRepository: LandownerRepository
+) {
+    suspend operator fun invoke(id: Int) = landownerRepository.getAndSaveLandDataHistory(id)
+}

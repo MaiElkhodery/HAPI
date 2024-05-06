aa692f29a3b86572c8461d1c05b4a442d412b35a
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-05-06 14:16:58 +0300

    remove land apis

--- app/src/main/java/com/example/hapi/data/remote/api/LandownerApiService.kt
+++ app/src/main/java/com/example/hapi/data/remote/api/LandownerApiService.kt
@@ -3,10 +3,9 @@ package com.example.hapi.data.remote.api
 import com.example.hapi.data.remote.request.SelectCropRequest
 import com.example.hapi.data.remote.response.CropRecommendationResponse
 import com.example.hapi.data.remote.response.LandDataResponse
-import com.example.hapi.data.remote.response.LandHistoryResponse
 import com.example.hapi.data.remote.response.LastFarmerResponse
+import com.example.hapi.util.FARMERS
 import com.example.hapi.util.LAND_DATA
-import com.example.hapi.util.LAND_HISTORY
 import com.example.hapi.util.LAST_FARMER
 import com.example.hapi.util.RECOMMENDATIONS
 import com.example.hapi.util.SELECTED_CROP
@@ -14,7 +13,6 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface LandownerApiService {
     @POST(RECOMMENDATIONS)
@@ -26,14 +24,13 @@ interface LandownerApiService {
         @Body crop: SelectCropRequest
     ): Response<Unit>
 
-    @GET(LAND_HISTORY)
-    suspend fun getLandHistory(
-        @Query("id") id: Int
-    ): Response<List<LandHistoryResponse>>
-
     @GET(LAND_DATA)
-    suspend fun getLandData(): Response<LandDataResponse>
+    suspend fun getTanksData(): Response<LandDataResponse>
 
     @GET(LAST_FARMER)
     suspend fun getLastFarmer():Response<LastFarmerResponse>
+
+    @GET(FARMERS)
+    suspend fun getFarmers():Response<List<String>>
+
 }

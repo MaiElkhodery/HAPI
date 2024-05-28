3521cd100b20398ba442f43063c04a4b5cd7a5ea
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-05-28 16:38:29 +0300

    modify text to use string resource

--- app/src/main/java/com/example/hapi/presentation/settings/common/LandIdRow.kt
+++ app/src/main/java/com/example/hapi/presentation/settings/common/LandIdRow.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.hapi.R
@@ -28,7 +29,7 @@ fun LandIdRow(
             tint = YellowAppColor,
             modifier = Modifier.padding(end = 6.dp)
         )
-        YellowExtraBoldText(size = 15, text = "LAND ID: $landId")
+        YellowExtraBoldText(size = 15, text = stringResource(id = R.string.land_id) + " : "+ landId)
     }
 }
 

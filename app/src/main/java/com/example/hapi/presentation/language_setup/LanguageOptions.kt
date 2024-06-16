79db137411036df58a16f679aec6259521dc41d9
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-06-16 16:26:09 +0300

    modify spaces

--- app/src/main/java/com/example/hapi/presentation/language_setup/LanguageOptions.kt
+++ app/src/main/java/com/example/hapi/presentation/language_setup/LanguageOptions.kt
@@ -1,8 +1,6 @@
 package com.example.hapi.presentation.language_setup
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,8 +9,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -21,17 +17,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hapi.R
-import com.example.hapi.presentation.common.ContinueButtonWithYellowBackground
 import com.example.hapi.ui.theme.DarkGreenAppColor
 import com.example.hapi.ui.theme.YellowAppColor
 import com.example.hapi.util.ARABIC
 import com.example.hapi.util.ENGLISH
 
 @Composable
-fun LanguageContent(
+fun LanguageOptions(
     modifier: Modifier = Modifier,
     isEnglishSelected: Boolean,
-//    isArabicSelected: StateFlow<Boolean>,
     onLanguageSelected: (String) -> Unit
 ) {
 
@@ -49,7 +43,7 @@ fun LanguageContent(
             ),
             text = stringResource(id = R.string.choose_language),
             textAlign = TextAlign.Center,
-            lineHeight = 10.sp
+            lineHeight = 8.sp
         )
         Text(
             color = YellowAppColor,
@@ -61,7 +55,7 @@ fun LanguageContent(
             ),
             text = stringResource(id = R.string.to_proceed),
             textAlign = TextAlign.Center,
-            lineHeight = 10.sp
+            lineHeight = 8.sp
         )
 
         Spacer(modifier = Modifier.height(14.dp))
@@ -99,49 +93,10 @@ fun LanguageContent(
     }
 }
 
-@Composable
-fun BottomSection(
-    modifier: Modifier = Modifier,
-    isLanguageSelected: Boolean,
-    onContinueClicked: () -> Unit
-) {
-//    if (isLanguageSelected) {
-
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        ContinueButtonWithYellowBackground {
-            onContinueClicked()
-        }
-        Spacer(modifier = Modifier.height(21.dp))
-        Box(
-            modifier = modifier,
-            contentAlignment = Alignment.BottomCenter
-        ) {
-            Image(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .fillMaxWidth(),
-                painter = painterResource(id = R.drawable.main_crop),
-                contentScale = ContentScale.Fit,
-                contentDescription = "crop"
-            )
-        }
-    }
-//    }else{
-//        CropImage(
-//            modifier = modifier
-//                .fillMaxWidth()
-//        )
-//    }
-}
-
 @Preview
 @Composable
 private fun LanguageContentPreview() {
-    LanguageContent (
+    LanguageOptions(
         isEnglishSelected = true,
-//        isArabicSelected = false
     ) { }
 }
--- app/src/main/java/com/example/hapi/presentation/language_setup/LanguageSetUp.kt
+++ app/src/main/java/com/example/hapi/presentation/language_setup/LanguageSetUp.kt
@@ -1,8 +1,9 @@
 package com.example.hapi.presentation.language_setup
 
-import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,16 +17,19 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.hapi.R
+import com.example.hapi.presentation.common.ContinueButtonWithYellowBackground
 import com.example.hapi.presentation.common.Logo
 import com.example.hapi.presentation.settings.language.LanguageViewModel
 import com.example.hapi.presentation.welcome.navigateToWelcomeScreen
 import com.example.hapi.ui.theme.GreenAppColor
-import com.example.hapi.util.Dimens
 import com.example.hapi.util.ENGLISH
 
 @Composable
@@ -48,8 +52,8 @@ fun LanguageSetUp(
     ) {
         val screenHeight = maxHeight
 
-        val topPadding = screenHeight * 0.1f
-        val contentMargin = if (screenHeight < 600.dp) 16.dp else Dimens.content_margin
+        val topPadding = screenHeight * 0.08f
+        val contentMargin = if (screenHeight < 600.dp) 12.dp else 18.dp
 
         Column(
             modifier = Modifier
@@ -58,6 +62,7 @@ fun LanguageSetUp(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceBetween
         ) {
+
             Spacer(modifier = Modifier.height(topPadding))
 
             Logo(
@@ -67,20 +72,31 @@ fun LanguageSetUp(
 
             Spacer(modifier = Modifier.height(contentMargin))
 
-            LanguageContent(
+            LanguageOptions(
                 isEnglishSelected = appLanguage == ENGLISH
             ) { language ->
-                Log.d("LanguageSetUp", "Language selected: $language")
                 onLanguageSelected(language)
                 languageViewModel.getLanguage()
             }
 
+            ContinueButtonWithYellowBackground {
+               navController.navigateToWelcomeScreen()
+            }
+
             Spacer(modifier = Modifier.height(contentMargin))
 
-            BottomSection(
-                isLanguageSelected = appLanguage == ENGLISH
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.BottomCenter
             ) {
-                navController.navigateToWelcomeScreen()
+                Image(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth(),
+                    painter = painterResource(id = R.drawable.main_crop),
+                    contentScale = ContentScale.Fit,
+                    contentDescription = "crop"
+                )
             }
         }
     }
